db8f6fcab03f1246b164a982856c42f641f5426a
EXY <107622230+hsumyatm7308@users.noreply.github.com>
2023-01-04 19:39:43 +0630

    1/4

--- app.js
+++ app.js
@@ -1,124 +1,121 @@
-// Get UI 
-const getaudioscreen = document.getElementById('audioscreen');
-const playbtn = document.getElementById('play'),
-prevbtn = document.getElementById('prev'),
-nextbtn = document.getElementById('next'),
-stopbtn = document.getElementById('stop');
+// Start DropDown-Container 
 
-const getprogressbtn = document.getElementById('progress-container'),
-progress = document.getElementById('progress');
+$(document).ready(function(){
+    var getbrowsedpd = document.getElementById('navbrowse');
+    var getcommtydpd = document.getElementById('navcomty');
+    var getwritedpd = document.getElementById('navwrite')
+    
+    var getdropdowncontainer = document.querySelector('.dropdown-container');
+    var getdropdowncontainer2 = document.querySelector('.dropdown-container2');
+    var getdropdowncontainer3 = document.querySelector('.dropdown-container3')
+    var brstoggledpd = document.querySelector('.togl');
+    
 
-const getvolprogress = document.getElementById('volumeprogress');
-const getdisplaytime = document.getElementById('displaytime')
+    var getbanner = document.querySelector('.banner');
 
-const audios = ['sample1','sample2','sample3'];
-
-let curridx = 0;
-
-loadaudio(audios[curridx]);
-
-function loadaudio(ado){
-    getaudioscreen.src = `./source/${ado}.mp3`;
-}
-
-function playado(){
-    playbtn.querySelector('i.fas').classList.remove('fa-play'); 
-    playbtn.querySelector('i.fas').classList.add('fa-pause');
-    getaudioscreen.play();
-}
-
-function pauseado(){
-    playbtn.querySelector('i.fas').classList.add('fa-play'); 
-    playbtn.querySelector('i.fas').classList.remove('fa-pause');
-    getaudioscreen.pause();
-}
-
-function playpauseado() {
-    if (getaudioscreen.paused) {
-        getaudioscreen.play();
-    } else {
-        getaudioscreen.pause();
-    }
-}
-
-
-function previousado(){
-    curridx--;
-
-    if(curridx < 0){
-        curridx = audios.length-1;
-    }
-
-    loadaudio(audios[curridx]);
-    playado();
-}
-
-function nextado(){
-    curridx++;
-    if(curridx > audios.length - 1){
-        curridx = 0;
-    }
-
-    loadaudio(audios[curridx]);
-    playado();
-}
-
-function stopado(){
-    getaudioscreen.currentTime = 0;
-    progress.value = getaudioscreen.currentTime;
-    pauseado();
-}
-
-function updateprogress(e){
-    const {currentTime} = e.target;
-    const {duration} = e.target;
-
-    if(getaudioscreen.currentTime === 0){
-        progress.style.widows = `0%`
+    $(getbrowsedpd).click(function(){
+        console.log('hei')
         
-    }else{
-        const progresspercent = (currentTime/duration)*100;
-        progress.style.width = `${progresspercent}%`;
-    }
+        $(getdropdowncontainer).toggle(brstoggledpd);
 
-    let mins = Math.floor(getaudioscreen.currentTime/60);
-    let secs =Math.floor(getaudioscreen.currentTime%60);
 
-    const minuteval = mins.toString().padStart(2,'0');
-    const secuteval = secs.toString().padStart(2,'0');
-    getdisplaytime.innerText = `${minuteval}:${secuteval}`
-}
+        $(getcommtydpd).click(function(){
+            $(getdropdowncontainer).css('display','none')
+        });
 
-function setaudioprogtess(e){
-    const width = this.clientWidth;
-    // console.log(width)
-    const clickx = e.offsetX;
-    // console.log(clickx)
 
-    const duration = getaudioscreen.duration;
-    getaudioscreen.currentTime = (clickx/width)*duration;
-}
+        $(`getbanner,.cover-bg`).click(function(){
+            $(getdropdowncontainer).css('display','none')
+        })
 
-function volumecontrol(){
-    console.log(getvolprogress.value)
-    // volume came from audio play 
-    getaudioscreen.volume = getvolprogress.value/100;
+    });
 
-    // 1 is default 
-    // 0.5 half volume 50%
-    // 0 is mute 0%
+    
+    $(getcommtydpd).click(function(){
+        console.log('hei')
+        
+        $(getdropdowncontainer2).toggle(brstoggledpd)
 
-}
+        $(getbrowsedpd).click(function(){
+            $(getdropdowncontainer2).css('display','none')
+        });
 
-getaudioscreen.addEventListener('timeupdate',updateprogress);
-getaudioscreen.addEventListener('play',playado);
-getaudioscreen.addEventListener('pause',pauseado);
+        $(getwritedpd).click(function(){
+            $(getdropdowncontainer2).css('display','none')
+        });
 
-playbtn.addEventListener('click',playpauseado);
+        $(`getbanner,.cover-bg`).click(function(){
+            $(getdropdowncontainer2).css('display','none')
+        })
 
-prevbtn.addEventListener('click',previousado);
-nextbtn.addEventListener('click',nextado);
-stopbtn.addEventListener('click',stopado);
 
-getprogressbtn.addEventListener('click',setaudioprogtess);
-getvolprogress.addEventListener('click',volumecontrol);
+    });
+    
+
+    $(getwritedpd).click(function(){
+        console.log('hei')
+        
+        $(getdropdowncontainer3).toggle(brstoggledpd)
+
+        $(getcommtydpd).click(function(){
+            $(getdropdowncontainer3).css('display','none')
+        });
+        $(getbrowsedpd).click(function(){
+            $(getdropdowncontainer3).css('display','none')
+        });
+
+        $(`getbanner,.cover-bg`).click(function(){
+            $(getdropdowncontainer3).css('display','none')
+        })
+    });
+
+
+    // start progress bar 
+    $(window).scroll(function(){
+        console.log('his');
+        var getprogress = document.querySelector('.progress-bar')
+        // console.log(getprogress)
+        var getscrolltop = $(this).scrollTop();
+        console.log(getscrolltop);
+
+        var getclientheight = $(window).height();
+        console.log(getclientheight)
+
+        var getscrollheight = $(document).height();
+        console.log(getscrollheight);
+
+
+        var getclaheight = getscrollheight-getclientheight
+        console.log(getclaheight)
+
+        var getfinalheight = (getscrolltop *100 )/ getclaheight 
+        console.log(getfinalheight);
+
+        getprogress.style.width = `${getfinalheight}%`
+    }); 
+
+    var followbtn = document.getElementsByClassName('followbtn'); 
+    $(followbtn).click(function(){
+        $(this).toggleClass('foled');
+
+       if($(this).hasClass('foled')){
+
+        $('.fl-text').html('Following')
+
+        $('.fl-icon').html('<i class="fa-regular fa-user-check"></i>')
+       }else{
+
+        $('.fl-text').html('Follow')
+        $('.fl-icon').html('<i class="fa-solid fa-user-plus"></i>')
+
+
+       }
+    })
+
+});
+
+
+
+
+
+
